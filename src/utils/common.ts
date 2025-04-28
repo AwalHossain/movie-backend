08f7e7bb795e69d5f6c89ce1ce5d24ac74b8c01d
Awalhossain <waqin21@gmail.com>
2025-04-28 16:06:25 +0600

    feat: Implement JWT token creation and verification functions in common utility

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,8 @@
+import config from "../config/index";
+
+import jwt from "jsonwebtoken";
+
+
 function getDbStatusText(status: number): string {
     switch (status) {
       case 0: return 'disconnected';
@@ -9,3 +14,25 @@ function getDbStatusText(status: number): string {
   }
 
   export { getDbStatusText };
+
+
+
+const createToken = (id: any) => {
+  if (!config.jwtSecret) {
+    throw new Error("JWT secret is not defined in the configuration");
+  }
+  const token = jwt.sign({ id }, config.jwtSecret, {
+    expiresIn: "7d",
+  });
+
+  return token;
+};
+
+const verifyToken = (token: string) => {
+  if (!config.jwtSecret) {
+    throw new Error("JWT secret is not defined in the configuration");
+  }
+  return jwt.verify(token, config.jwtSecret);
+};
+
+export { createToken, verifyToken };
