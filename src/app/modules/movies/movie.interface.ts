512c778ba19da064b548dbfc4461d23c250561e3
Awalhossain <waqin21@gmail.com>
2025-04-29 22:58:33 +0600

    feat: Add movie interface and related types for movie management

---
+++ src/app/modules/movies/movie.interface.ts
@@ -0,0 +1,57 @@
+import { Document } from 'mongoose';
+
+export interface ICastMember {
+  name?: string | null;
+  character?: string | null;
+  profile_path?: string | null;
+}
+
+export interface IMovieFilters {
+  searchTerm?: string;
+  sourceCategory?: string;
+  genre?: string;
+}
+
+
+
+export interface IMoviePaginationOptions {
+  page?: number;
+  limit?: number;
+  sortBy?: string;
+  sortOrder?: string;
+  skip?: number;
+}
+
+export interface IMovie {
+  tmdb_id: number;
+  title: string;
+  overview?: string | null;
+  release_date?: Date | null;
+  tmdb_rating?: number | null;
+  tmdb_vote_count?: number | null;
+  runtime?: number | null;
+  genres: string[];
+  poster?: string | null;
+  backdrop?: string | null;
+  rating?: number | null;
+  vote_count?: number | null;
+  cast: ICastMember[];
+  popularity?: number | null;
+  sourceCategory?: string | null;
+  averageUserRating?: number | null;
+  userReviewCount?: number | null;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface IMovies {
+  meta: {
+    total: number;
+    page: number;
+    limit: number;
+  };
+  data: IMovie[];
+}
+
+
+export interface IMovieSchema extends IMovie, Document { }
