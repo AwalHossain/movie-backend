175b885249613ce66ba7d46c8a1fc037f7c81443
Awalhossain <waqin21@gmail.com>
2025-04-29 22:59:13 +0600

    feat: Add movie routes for CRUD operations including authentication middleware

---
+++ src/app/modules/movies/movie.route.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import isAuthenticated from "../../middlewares/isAuthenticated";
+import { MovieController } from "./movie.controller";
+
+const router = express.Router();
+
+router.post(
+  "/",
+  isAuthenticated,
+  MovieController.addMovie
+);
+
+
+router.get("/", MovieController.getAllMovies);
+router.get("/id/:id", MovieController.getMovieById);
+router.get("/tmdb/:tmdbId", MovieController.getMovieByTmdbId);
+
+router.delete(
+  "/:id",
+  isAuthenticated,
+  MovieController.deleteMovieById
+);
+
+export const MovieRoutes = router; 
