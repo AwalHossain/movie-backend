f9c5cd1e4acfe5c2da4396c3353ead49d15832a7
Awalhossain <waqin21@gmail.com>
2025-04-29 22:44:16 +0600

    feat: Add movie and review routes to the main router for improved API structure

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,27 @@
 import { Router } from "express";
+import { MovieRoutes } from "../app/modules/movies/movie.route";
+import { ReviewRoutes } from "../app/modules/reviews/review.route";
 import { UserRoutes } from "../app/modules/user/user.route";
-const router = Router();
 
+const router = Router();
 
 const ModuleRoutes = [
     {
         path: "/auth",
         route: UserRoutes,
+    },
+    {
+        path: "/movies",
+        route: MovieRoutes,
+    },
+    {
+        path: "/reviews",
+        route: ReviewRoutes,
     }
-  ];
+];
   
-  ModuleRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
+ModuleRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
   
-  export default router;
+export default router;
